7cf797a9f775e9a0810ed9b03fd77ffdf42e6f9a
Joris Blaak <joris@label305.com>
2019-01-26 15:44:46 +0100

    Removed samples
    Will add new versions soon that are runnable

--- samples/mvvm/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './ui/misc/index.css';
-import {MyNavigator} from './navigator';
-import {Squirrel} from '../../src/react/squirrel';
-
-const myNavigator = new MyNavigator();
-
-ReactDOM.render(<Squirrel navigator={myNavigator} />, document.getElementById('root'));
