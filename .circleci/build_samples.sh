80207c2df71232531940f9fdacfecf301c852ea6
Joris Blaak <joris@label305.com>
2019-01-28 19:04:57 +0100

    Added Mvvm React sample

--- .circleci/build_samples.sh
+++ .circleci/build_samples.sh
@@ -3,3 +3,7 @@
 yarn --cwd "./samples/react/1. Basic" install 
 yarn --cwd "./samples/react/1. Basic" start
 yarn --cwd "./samples/react/1. Basic" test
+
+yarn --cwd "./samples/react/2. Mvvm" install 
+yarn --cwd "./samples/react/2. Mvvm" start
+yarn --cwd "./samples/react/2. Mvvm" test
