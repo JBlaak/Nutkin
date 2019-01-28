a50a2d1fcbecfc504b3a0f9cf1bf581a0c470a86
Joris Blaak <joris@label305.com>
2019-01-28 19:30:15 +0100

    Added Stack React sample

--- .circleci/build_samples.sh
+++ .circleci/build_samples.sh
@@ -7,3 +7,7 @@ yarn --cwd "./samples/react/1. Basic" test
 yarn --cwd "./samples/react/2. Mvvm" install 
 yarn --cwd "./samples/react/2. Mvvm" start
 yarn --cwd "./samples/react/2. Mvvm" test
+
+yarn --cwd "./samples/react/3. Stack" install 
+yarn --cwd "./samples/react/3. Stack" start
+yarn --cwd "./samples/react/3. Stack" test
