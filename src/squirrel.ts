acdd160dc826ffe0c1c7744829a540344da4545f
Joris Blaak <joris@label305.com>
2019-02-18 14:33:59 +0100

    Added build configuration (#14)

---
+++ src/squirrel.ts
@@ -0,0 +1,11 @@
+/* Navigators */
+import * as Navigators from './core/navigators';
+export {Navigator} from './core/navigator';
+export {Navigators};
+
+/* Scenes */
+
+export {Scene} from './core/scene';
+
+/* React */
+export {Squirrel} from './react/squirrel';
